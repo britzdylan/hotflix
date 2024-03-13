dcad1da4a31f7d045c61aa70914fcdf489079f34
Dylan <britzdylan@gmail.com>
2024-03-13 15:42:54 +0100

    Delete Header component

--- src/components/Base/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <header
-    class="p-4 flex items-center border-b sticky top-0 h-[80px] bg-slate-100 bg-opacity-50 bg-blur z-10"
-  >
-    <h1 class="font-semibold text-xl">For Dylan</h1>
-  </header>
-</template>
-
-<script setup lang="ts"></script>
