f55e1ed2d2edaf7cb9604c1ec67a47f1e4a27642
Dylan <britzdylan@gmail.com>
2024-03-13 15:43:20 +0100

    Add Header component with search icon

---
+++ src/components/Header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <header
+    class="p-4 flex items-center justify-between sticky top-0 h-[80px] bg-slate-900 text-white bg-opacity-50 bg-blur z-10 rounded-b-xl"
+  >
+    <h1 class="font-semibold text-xl">For Dylan</h1>
+    <IconsSearch />
+  </header>
+</template>
+
+<script setup lang="ts">
+import IconsSearch from '@/components/Icons/Search.vue'
+</script>
+<!-- https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg -->
