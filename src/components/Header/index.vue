d6efb88fd59c8bbc7526b11f7b656cd19786adab
Dylan <britzdylan@gmail.com>
2024-03-13 16:27:39 +0100

    Refactor Header component to use BaseHeader

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <header
-    class="p-4 flex items-center justify-between sticky top-0 h-[80px] bg-neutral-950 text-white bg-opacity-90 bg-blur z-10 rounded-b-xl"
-  >
+  <BaseHeader>
     <h1 class="font-semibold text-xl">For Dylan</h1>
     <IconsSearch />
-  </header>
+  </BaseHeader>
 </template>
 
 <script setup lang="ts">
 import IconsSearch from '@/components/Icons/Search.vue'
+import BaseHeader from './Base.vue'
 </script>
 <!-- https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg -->
