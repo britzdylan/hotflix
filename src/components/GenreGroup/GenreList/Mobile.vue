e442126d0ede9b6ae26a5cde47ba14397b23ed19
Dylan <britzdylan@gmail.com>
2024-03-13 15:43:30 +0100

    Refactor GenreList/Mobile.vue component

--- src/components/GenreGroup/GenreList/Mobile.vue
+++ src/components/GenreGroup/GenreList/Mobile.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="flex gap-4 snap-proximity snap-x overflow-x-auto no-scrollbar">
-    <div
-      v-for="k in 10"
-      :key="k + '_show'"
-      class="w-[200px] h-[300px] bg-slate-200 rounded-md snap-start lg:snap-end shrink-0"
-    ></div>
+    <ShowCard v-for="k in 10" :key="k + '_show'"></ShowCard>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ShowCard from '@/components/Show/Card.vue'
+</script>
