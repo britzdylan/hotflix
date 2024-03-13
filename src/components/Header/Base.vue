6ee76404061954a8943eae1c8464e3760d65baf5
Dylan <britzdylan@gmail.com>
2024-03-13 16:27:36 +0100

    Add Base.vue component to Header

---
+++ src/components/Header/Base.vue
@@ -0,0 +1,7 @@
+<template>
+  <header
+    class="p-4 flex items-center justify-between sticky top-0 h-[80px] bg-neutral-950 text-white bg-opacity-90 bg-blur z-10 rounded-b-xl"
+  >
+    <slot />
+  </header>
+</template>
