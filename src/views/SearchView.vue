41fbb9d6d5e294ea79d3d6f48b92bb7e2c5bcc0d
Dylan <britzdylan@gmail.com>
2024-03-13 16:34:51 +0100

    Add loading SVG icon and implement loading state in SearchView

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,17 +1,24 @@
-<script setup lang="ts">
-import BasePageViewMobile from '@/components/Base/PageView/Mobile.vue'
-import ShowCard from '@/components/Show/Card.vue'
-import HeaderSearch from '@/components/Header/Search.vue'
-</script>
-
 <template>
   <BasePageViewMobile>
     <template #header>
       <HeaderSearch />
     </template>
     <h2 class="text-xl font-semibold">Recommended tv shows</h2>
-    <div class="grid grid-cols-2 gap-2">
-      <ShowCard class="w-full" v-for="i in 10" :key="i + '_show'"></ShowCard>
+    <div v-if="!loading" class="grid grid-cols-3 gap-2">
+      <ShowCard class="w-full" v-for="i in 12" :key="i + '_show'"></ShowCard>
+    </div>
+    <div v-else class="flex justify-center items-center h-[calc(100vh-200px)]">
+      <Loader />
     </div>
   </BasePageViewMobile>
 </template>
+
+<script setup lang="ts">
+import BasePageViewMobile from '@/components/Base/PageView/Mobile.vue'
+import ShowCard from '@/components/Show/Card.vue'
+import HeaderSearch from '@/components/Header/Search.vue'
+import Loader from '@/components/Icons/Loading.vue'
+import { ref } from 'vue'
+
+const loading = ref(false)
+</script>
