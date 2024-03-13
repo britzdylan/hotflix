0ae909a217a86ed7c907ccbd819544c9060d2ba1
Dylan <britzdylan@gmail.com>
2024-03-13 15:43:47 +0100

    Update imports and add NavbarMobile component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import TheHeader from '@/components/Base/Header.vue'
+import Header from '@/components/Header/index.vue'
 import GenreGroup from '@/components/GenreGroup/index.vue'
+import NavbarMobile from '@/components/Navbar/Mobile.vue'
 import { ref, onMounted } from 'vue'
 </script>
 
 <template>
   <main class="bg-white">
-    <TheHeader />
+    <Header />
     <section class="p-4 flex flex-col gap-4">
       <GenreGroup v-for="i in 10" />
     </section>
+    <NavbarMobile />
   </main>
 </template>
