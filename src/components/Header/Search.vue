8a980a6a216b65afee3b9f7e2b3d14321ac59b22
Dylan <britzdylan@gmail.com>
2024-03-13 16:27:43 +0100

    Add Search component to Header

---
+++ src/components/Header/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseHeader class="gap-4">
+    <IconsChevron class="rotate-90" />
+    <input
+      type="text"
+      class="bg-neutral-700 placeholder:text-neutral-400 border-none text-white w-full px-4 py-3 rounded-md outline-none"
+      placeholder="Search for Names, Genres. & People"
+    />
+  </BaseHeader>
+</template>
+
+<script setup lang="ts">
+import IconsChevron from '@/components/Icons/Chevron.vue'
+import BaseHeader from './Base.vue'
+</script>
