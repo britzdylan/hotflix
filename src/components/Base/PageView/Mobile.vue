829f758c2651546322d2be25ebcc402c63cf13e6
Dylan <britzdylan@gmail.com>
2024-03-13 16:27:22 +0100

    Add Mobile PageView component

---
+++ src/components/Base/PageView/Mobile.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <slot name="header"> </slot>
+    <section class="p-4 pb-[85px] flex flex-col gap-4">
+      <slot />
+    </section>
+    <slot name="footer"> </slot>
+  </main>
+</template>
