050e55a638104f6d6c26922ae4c0c34d6f04f749
Dylan <britzdylan@gmail.com>
2024-03-13 16:27:59 +0100

    Refactor HomeView.vue to use BasePageViewMobile component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
+import BasePageViewMobile from '@/components/Base/PageView/Mobile.vue'
 import Header from '@/components/Header/index.vue'
 import GenreGroup from '@/components/GenreGroup/index.vue'
 import NavbarMobile from '@/components/Navbar/Mobile.vue'
-import { ref, onMounted } from 'vue'
 </script>
 
 <template>
-  <main class="bg-neutral-950 text-white">
-    <Header />
-    <section class="p-4 pb-[85px] flex flex-col gap-4">
-      <GenreGroup v-for="i in 10" />
-    </section>
-    <NavbarMobile />
-  </main>
+  <BasePageViewMobile>
+    <template #header>
+      <Header />
+    </template>
+
+    <GenreGroup v-for="i in 10" />
+
+    <template #footer>
+      <NavbarMobile />
+    </template>
+  </BasePageViewMobile>
 </template>
