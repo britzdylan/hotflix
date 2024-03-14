34f21855fb6f3d93df33e276904788cc5491634b
Dylan <britzdylan@gmail.com>
2024-03-14 11:18:51 +0100

    Refactor HomeView component imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import BasePageViewMobile from '@/components/Base/PageView/Mobile.vue'
+import BasePageView from '@/components/Base/PageView/index.vue'
 import Header from '@/components/Header/index.vue'
 import GenreGroup from '@/components/GenreGroup/index.vue'
 import NavbarMobile from '@/components/Navbar/Mobile.vue'
 </script>
 
 <template>
-  <BasePageViewMobile>
+  <BasePageView>
     <template #header>
       <Header />
     </template>
@@ -16,5 +16,5 @@ import NavbarMobile from '@/components/Navbar/Mobile.vue'
     <template #footer>
       <NavbarMobile />
     </template>
-  </BasePageViewMobile>
+  </BasePageView>
 </template>
