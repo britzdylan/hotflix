b053391db75df591a3469660a282deb155ab5b84
Dylan <britzdylan@gmail.com>
2024-03-14 11:18:57 +0100

    Refactor BasePageViewMobile to BasePageView in SearchView.vue

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,5 @@
 <template>
-  <BasePageViewMobile>
+  <BasePageView>
     <template #header>
       <HeaderSearch />
     </template>
@@ -7,14 +7,14 @@
     <div v-if="!loading" class="grid grid-cols-3 gap-2">
       <ShowCard class="w-full" v-for="i in 12" :key="i + '_show'"></ShowCard>
     </div>
-    <div v-else class="flex justify-center items-center h-[calc(100vh-200px)]">
+    <div v-else class="flex justify-center items-center flex-grow h-full">
       <Loader />
     </div>
-  </BasePageViewMobile>
+  </BasePageView>
 </template>
 
 <script setup lang="ts">
-import BasePageViewMobile from '@/components/Base/PageView/Mobile.vue'
+import BasePageView from '@/components/Base/PageView/index.vue'
 import ShowCard from '@/components/Show/Card.vue'
 import HeaderSearch from '@/components/Header/Search.vue'
 import Loader from '@/components/Icons/Loading.vue'
