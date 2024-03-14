20c6f91bc2be6d72606715c13967924741145d7e
Dylan <britzdylan@gmail.com>
2024-03-14 11:18:30 +0100

    Add PageView component

---
+++ src/components/Base/PageView/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="flex flex-col min-h-screen">
+    <slot name="header"></slot>
+    <section class="p-4 flex flex-col gap-4 flex-grow">
+      <slot />
+    </section>
+    <slot name="footer"></slot>
+  </main>
+</template>
