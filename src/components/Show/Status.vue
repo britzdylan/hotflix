e39a706131f4cb862fa1ccd5072a2de4bc9746f4
Dylan <britzdylan@gmail.com>
2024-03-15 15:17:07 +0100

    Add Status component

---
+++ src/components/Show/Status.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="inline-flex items-center gap-2">
+    <div :class="['h-2 w-2 rounded-full', showStatusColor]"></div>
+    <span class="text-sm">{{ showStatus }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+interface IProps {
+  status: string | null
+}
+
+const props = defineProps<IProps>()
+
+const showStatus = computed(() => {
+  return props.status ? 'Ended' : 'Running'
+})
+
+const showStatusColor = computed(() => {
+  return props.status ? 'bg-yellow-500' : 'bg-green-500'
+})
+</script>
