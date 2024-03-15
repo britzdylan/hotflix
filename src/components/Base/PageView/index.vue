6fecf13d2e28c5bbf6fb7eadb38adf371c3af1e2
Dylan <britzdylan@gmail.com>
2024-03-16 00:14:03 +0100

    Refactor PageView component styles

--- src/components/Base/PageView/index.vue
+++ src/components/Base/PageView/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex flex-col min-h-screen">
     <slot name="header"></slot>
-    <section class="p-4 flex flex-col gap-4 flex-grow">
+    <section class="p-4 lg:px-12 2xl:px-0 flex flex-col gap-4 lg:gap-12 flex-grow">
       <slot />
     </section>
     <slot name="footer"></slot>
