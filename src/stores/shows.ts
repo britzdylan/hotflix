2c3ccea2bae90845938fbdcb2f041e3f95e1bee4
Dylan <britzdylan@gmail.com>
2024-03-15 16:43:55 +0100

    Add network, webChannel, seasons, and images to show details

--- src/stores/shows.ts
+++ src/stores/shows.ts
@@ -32,7 +32,11 @@ export const useShowsStore = defineStore('showStore', {
         officialSite: rawData.officialSite,
         premiered: rawData.premiered,
         ended: rawData.ended,
-        language: rawData.language
+        language: rawData.language,
+        network: rawData.network?.name ?? null,
+        webChannel: rawData.webChannel?.name ?? null,
+        seasons: rawData.seasons,
+        images: rawData.images
       } as IShowDetailed
     },
     cacheShow(show: IShowDetailed) {
@@ -64,7 +68,19 @@ export const useShowsStore = defineStore('showStore', {
       if (cachedShow) {
         return cachedShow.data
       }
-      const rawData = await getData(`shows/${id}`)
+      const rawShowData = await getData(`shows/${id}`)
+      const rawSeasonsData = await getData(`shows/${id}/seasons`)
+      const rawImagesData = await getData(`shows/${id}/images`)
+      const rawData = {
+        ...rawShowData,
+        seasons: rawSeasonsData.length,
+        images: rawImagesData
+          .map((item: any) => ({
+            type: item.type,
+            image: item.resolutions.original.url
+          }))
+          .filter((item: any) => item.type !== null)
+      }
       const preparedData = this.prepareShowData(rawData)
       this.cacheShow(preparedData)
       return preparedData as IShowDetailed
