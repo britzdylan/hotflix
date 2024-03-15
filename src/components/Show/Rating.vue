85f5d4248f0138ff33c8199ddedb2095ce801dc9
Dylan <britzdylan@gmail.com>
2024-03-15 15:17:01 +0100

    Add Rating component to show the rating of a show

---
+++ src/components/Show/Rating.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inline-flex items-start">
+    <IconsStar />
+
+    <span class="text-2xl font-semibold ml-1 leading-6">{{ rating }}</span>
+    <span class="text-sm text-neutral-300 leading-4">/10</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconsStar from '@/components/Icons/Star.vue'
+
+interface IProps {
+  rating: number
+}
+defineProps<IProps>()
+</script>
