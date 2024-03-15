292e1362a409f46d18b409870bafa37cb5b933b7
Dylan <britzdylan@gmail.com>
2024-03-15 11:11:55 +0100

    Refactor Card.vue component

--- src/components/Show/Card.vue
+++ src/components/Show/Card.vue
@@ -3,23 +3,19 @@
     class="aspect-[210/295] bg-neutral-200 rounded-md overflow-hidden snap-start lg:snap-end shrink-0"
   >
     <img
-      src="https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg"
-      alt="Show Image"
+      v-if="image"
+      loading="lazy"
+      :src="image"
+      :alt="name"
       class="w-full object-cover object-center"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import type { IShow } from '@/types'
 
-interface Props {
-  size: 'large' | 'small'
-}
-
-const props = withDefaults(defineProps<Props>(), {
-  size: 'small'
-})
+defineProps<IShow>()
 </script>
 
 <style scoped></style>
