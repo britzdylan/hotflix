5b2757007d83c3b3e20bb0e3b7e0974d2dc22691
Dylan <britzdylan@gmail.com>
2024-03-15 16:43:45 +0100

    Refactor ShowDetails.vue component

--- src/components/Show/ShowDetails.vue
+++ src/components/Show/ShowDetails.vue
@@ -1,12 +1,21 @@
 <template>
   <h2 class="text-lg font-semibold">{{ name }}</h2>
-  <div class="flex gap-2 my-1">
+  <div class="flex gap-2 my-1 items-center">
     <span class="text-sm text-neutral-300">{{ language }}</span>
+    <div class="w-1 h-1 rounded-full bg-neutral-600"></div>
     <span class="text-sm text-neutral-300">{{ premieredYear }}</span>
-    <span class="text-sm text-neutral-300">CBS</span>
-    <span class="text-sm text-neutral-200">3 seasons</span>
+    <div class="w-1 h-1 rounded-full bg-neutral-600"></div>
+    <span class="text-sm text-neutral-300">{{ channel }}</span>
+    <div class="w-1 h-1 rounded-full bg-neutral-600"></div>
+    <span class="text-sm text-neutral-200">{{ seasons }} seasons</span>
   </div>
-  <a :href="officialSite" target="_blank" rel="noopener noreferrer" class="mb-3 w-full">
+  <a
+    v-if="officialSite"
+    :href="officialSite"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="mb-3 w-full"
+  >
     <BaseButton> Official Site </BaseButton>
   </a>
   <button
@@ -25,14 +34,21 @@ interface IProps {
   name: string
   language: string
   premiered: string
-  officialSite: string
+  officialSite: string | null
   summary: string
+  seasons: number
+  webChannel: string | null
+  network: string | null
 }
 
 const props = defineProps<IProps>()
 
 const premieredYear = computed(() => {
   return new Date(props.premiered).getFullYear()
+})
+
+const channel = computed(() => {
+  return props.webChannel || props.network
 })
 </script>
 
