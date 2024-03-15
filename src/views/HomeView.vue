743a77e0497a3a9db3515420e345a0a375c68483
Dylan <britzdylan@gmail.com>
2024-03-15 11:11:37 +0100

    Refactor HomeView.vue to use pinia store for shows data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,31 @@
-<script setup lang="ts">
-import BasePageView from '@/components/Base/PageView/index.vue'
-import Header from '@/components/Header/index.vue'
-import GenreGroup from '@/components/GenreGroup/index.vue'
-import NavbarMobile from '@/components/Navbar/Mobile.vue'
-</script>
-
 <template>
   <BasePageView>
     <template #header>
       <Header />
     </template>
 
-    <GenreGroup v-for="i in 10" />
+    <GenreGroup
+      v-for="i in getGenres"
+      v-bind="{
+        genre: i,
+        shows: getShowsByGenre(i)
+      }"
+    />
 
     <template #footer>
       <NavbarMobile />
     </template>
   </BasePageView>
 </template>
+
+<script setup lang="ts">
+import BasePageView from '@/components/Base/PageView/index.vue'
+import Header from '@/components/Header/index.vue'
+import GenreGroup from '@/components/GenreGroup/index.vue'
+import NavbarMobile from '@/components/Navbar/Mobile.vue'
+import { useShowsStore } from '@/stores/shows'
+import { storeToRefs } from 'pinia'
+
+const showsStore = useShowsStore()
+const { getGenres, getShowsByGenre } = storeToRefs(showsStore)
+</script>
