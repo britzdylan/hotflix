c6d5ac6260695fe1ec86d0353bbb273c46790040
Dylan <britzdylan@gmail.com>
2024-03-15 11:12:35 +0100

    Refactor GenreGroup component to use dynamic genre and shows props

--- src/components/GenreGroup/index.vue
+++ src/components/GenreGroup/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="flex flex-col gap-2 last:pb-[85px]">
-    <h2 class="text-lg font-semibold leading-3">Drama</h2>
-    <GenreListMobile v-if="windowWidth <= 768" />
+    <h2 class="text-lg font-semibold leading-3">{{ genre }}</h2>
+    <GenreListMobile v-if="windowWidth <= 768" :shows="shows" />
     <GenreListDesktop v-else />
   </div>
 </template>
 
 <script setup lang="ts">
+interface props {
+  genre: string
+  shows: IShow[]
+}
+
 import { ref } from 'vue'
 import GenreListMobile from './GenreList/Mobile.vue'
 import GenreListDesktop from './GenreList/Desktop.vue'
+import type { IShow } from '@/types'
 
+defineProps<props>()
 const windowWidth = ref(window.innerWidth)
 </script>
 
