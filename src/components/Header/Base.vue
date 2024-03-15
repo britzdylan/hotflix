9137e22232abe512320f5c60e8e48b6b03a8a53c
Dylan <britzdylan@gmail.com>
2024-03-16 00:15:29 +0100

    Update header styling in Base.vue

--- src/components/Header/Base.vue
+++ src/components/Header/Base.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="p-4 flex items-center justify-between sticky top-0 h-[80px] bg-neutral-950 text-white bg-opacity-90 bg-blur z-10 rounded-b-xl"
+    class="p-4 lg:px-12 2xl:px-0 flex items-center justify-between sticky top-0 h-[80px] bg-neutral-950 border-b border-neutral-800 text-white z-10 rounded-b-xl lg:rounded-none"
   >
     <slot />
   </header>
