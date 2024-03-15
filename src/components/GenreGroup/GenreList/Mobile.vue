34e0119acd0e397bbaf474e26e8dc5c2fa0581fb
Dylan <britzdylan@gmail.com>
2024-03-15 11:12:09 +0100

    Refactor GenreList component to use RouterLink for show navigation

--- src/components/GenreGroup/GenreList/Mobile.vue
+++ src/components/GenreGroup/GenreList/Mobile.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="flex gap-2 snap-proximity snap-x overflow-x-auto no-scrollbar">
-    <ShowCard class="w-[105px]" v-for="k in 10" :key="k + '_show'"></ShowCard>
+    <RouterLink v-for="item in shows" :key="item.id + '_show'" :to="`shows/${item.id}`">
+      <ShowCard class="w-[105px]" v-bind="item"></ShowCard>
+    </RouterLink>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { IShow } from '@/types'
 import ShowCard from '@/components/Show/Card.vue'
+interface props {
+  shows: IShow[]
+}
+defineProps<props>()
 </script>
