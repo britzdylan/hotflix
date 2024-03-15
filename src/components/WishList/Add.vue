29d35502bad03fdbe861e1145227a4e60b72231f
Dylan <britzdylan@gmail.com>
2024-03-16 00:53:13 +0100

    Add WishList component with functionality to add and remove shows from watch list

---
+++ src/components/WishList/Add.vue
@@ -0,0 +1,33 @@
+<template>
+  <BaseButton
+    @click="handleWishListListing"
+    class="!bg-transparent !border !border-white text-white"
+  >
+    {{ buttonTitle }}
+  </BaseButton>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useWatchListStore } from '@/stores/watchList'
+
+const { addToWatchList, WatchList, removeFromWatchList } = useWatchListStore()
+interface IProps {
+  showId: number
+}
+
+const props = defineProps<IProps>()
+
+const isAdded = computed(() => WatchList.includes(props.showId))
+const buttonTitle = computed(() => (isAdded.value ? 'Remove from watch list' : 'Add to watch list'))
+
+const handleWishListListing = () => {
+  if (isAdded.value) {
+    removeFromWatchList(props.showId)
+  } else {
+    addToWatchList(props.showId)
+  }
+}
+</script>
+
+<style scoped></style>
