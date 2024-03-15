291c0c3cb4f1e3e0f8a8ddfbfac9b67addf54d28
Dylan <britzdylan@gmail.com>
2024-03-15 15:16:53 +0100

    Add Button component

---
+++ src/components/Base/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button
+    class="w-full h-[40px] p-4 flex items-center justify-center text-sm font-semibold bg-white rounded-md text-black"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
