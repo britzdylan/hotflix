4222024166c47f766fc783090b705d72825d41a7
Dylan <britzdylan@gmail.com>
2024-03-16 00:16:01 +0100

    Refactor GenreList component buttons

--- src/components/GenreGroup/GenreList/Desktop.vue
+++ src/components/GenreGroup/GenreList/Desktop.vue
@@ -1,56 +1,33 @@
 <!-- arrow left -->
 
 <template>
-  <div ref="slider" class="group overflow-hidden">
+  <div ref="slider" class="group overflow-hidden relative">
     <!-- Button Start -->
-    <div
+    <button
       @click="prevSlide"
       v-show="currentIndex > 0"
-      class="hidden group-hover:flex group-hover:opacity-50 hover:!opacity-80 transition-all duration-300 ease-in-out w-[70px] h-[300px] bg-white rounded-md absolute z-10 items-center justify-center"
+      class="group group-hover:flex slider-button left-0"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
-        />
-      </svg>
-    </div>
+      <IconsChevronDouble />
+    </button>
     <!-- Button end -->
     <!-- Button Start -->
-    <div
-      @click="nextSlide"
-      class="hidden group-hover:flex group-hover:opacity-50 hover:!opacity-80 transition-all duration-300 ease-in-out w-[70px] h-[300px] bg-white rounded-md snap-start shrink-0 absolute z-10 right-0 items-center justify-center"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
-        />
-      </svg>
-    </div>
+    <button @click="nextSlide" class="group group-hover:flex slider-button right-0">
+      <IconsChevronDouble class="rotate-180" />
+    </button>
     <!-- Button end -->
     <div
       :style="{ transform: `translateX(-${currentIndex * slideWidth}px)` }"
       class="flex gap-4 relative transition-all ease-in-out duration-500"
     >
       <!-- Item Start -->
-      <ShowCard size="large" v-for="k in 10" :key="k + '_show'"></ShowCard>
+      <ShowCard
+        @click="showPopup(`show_${k.id}`)"
+        class="cursor-pointer hover:scale-95 hover:outline outline-2 outline-offset-1 outline-neutral-50 transition-transform duration-300 ease-out"
+        v-for="k in shows"
+        v-bind="k"
+        :key="k + '_show'"
+      ></ShowCard>
       <!-- Item end -->
     </div>
   </div>
@@ -59,10 +36,20 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import ShowCard from '@/components/Show/Card.vue'
+import IconsChevronDouble from '@/components/Icons/ChevronDouble.vue'
+import type { IShow } from '@/types'
+import { usePopup } from '@/composables/popup'
+
+interface props {
+  shows: IShow[]
+}
+const props = defineProps<props>()
+
+const { showPopup } = usePopup()
+
 // State to track current slide index
 const currentIndex = ref(0)
-
-const items = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+const slideWidth = 200 + 16
 
 // Assuming 'container' is the container element of your carousel
 const slider = ref<HTMLAnchorElement>()
@@ -74,18 +61,11 @@ onMounted(() => {
   slidesVisible.value = Math.floor(sliderContainerWidth.value / slideWidth)
 })
 
-// Width of each slide including padding
-const slideWidth = 200 + 16
-
-// Calculate the number of slides that can fit within the available width
-
-// Method to move to the next slide
 const nextSlide = () => {
-  if (currentIndex.value + slidesVisible.value >= items.value.length) {
+  if (currentIndex.value + slidesVisible.value >= props.shows.length) {
     currentIndex.value = 0
     return
   }
-  //   currentIndex.value = (currentIndex.value + slidesVisible.value) % items.value.length
   currentIndex.value++
 }
 
@@ -95,4 +75,8 @@ const prevSlide = () => {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.slider-button {
+  @apply hidden  transition-all duration-300 ease-in-out w-[70px] h-[300px] cursor-pointer bg-neutral-950/50 rounded-md snap-start shrink-0 absolute z-10  items-center justify-center;
+}
+</style>
