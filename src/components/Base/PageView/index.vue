4d13d26d0e773490555abf8317f97cce538b8d11
Dylan <britzdylan@gmail.com>
2024-03-16 01:27:23 +0100

    Add DesktopShowDetails component to PageView

--- src/components/Base/PageView/index.vue
+++ src/components/Base/PageView/index.vue
@@ -1,5 +1,7 @@
 <template>
   <main class="flex flex-col min-h-screen">
+    <DesktopShowDetails v-if="showDesktopShowDetails" />
+
     <slot name="header"></slot>
     <section class="p-4 lg:px-12 2xl:px-0 flex flex-col gap-4 lg:gap-12 flex-grow">
       <slot />
@@ -7,3 +9,15 @@
     <slot name="footer"></slot>
   </main>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { usePopup } from '@/composables/popup'
+import DesktopShowDetails from '@/components/Show/DesktopShowDetails.vue'
+
+const { popup } = usePopup()
+
+const showDesktopShowDetails = computed(() => {
+  return popup.value && popup.value.includes('show_') && window.innerWidth > 768
+})
+</script>
