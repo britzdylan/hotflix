ff9ca54b7349b8495c315995cd72e5abc240e8f3
Dylan <britzdylan@gmail.com>
2024-03-17 00:47:08 +0100

    Add preparation of most popular shows to onBeforeMount hook

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { useWatchListStore } from './stores/watchList'
+import { useShowsStore } from './stores/shows'
 import { onBeforeMount } from 'vue'
 
 onBeforeMount(() => {
   const { initWatchList } = useWatchListStore()
+  const { prepareMostPopularShows } = useShowsStore()
   initWatchList()
+  prepareMostPopularShows()
 })
 </script>
 
