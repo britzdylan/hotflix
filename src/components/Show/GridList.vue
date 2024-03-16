efac2bcd75855e548c1ad5fa758766809240f06f
Dylan <britzdylan@gmail.com>
2024-03-16 01:44:46 +0100

    Add GridList component for displaying shows

---
+++ src/components/Show/GridList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="grid grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4">
+    <ShowCard
+      class="cursor-pointer hover:scale-95 hover:outline outline-2 outline-offset-1 outline-neutral-50 transition-transform duration-300 ease-out"
+      @click="navigateToShow(i.id)"
+      v-for="i in shows"
+      :key="i + '_show'"
+      v-bind="i"
+    ></ShowCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { usePopup } from '@/composables/popup'
+import ShowCard from '@/components/Show/Card.vue'
+import type { IShow } from '@/types'
+
+interface IProps {
+  shows: IShow[]
+}
+
+defineProps<IProps>()
+
+const { showPopup } = usePopup()
+const router = useRouter()
+
+const navigateToShow = (id: number) => {
+  if (window.innerWidth > 768) {
+    showPopup(`show_${id}`)
+  } else {
+    router.push(`/shows/${id}`)
+  }
+}
+</script>
+
+<style scoped></style>
