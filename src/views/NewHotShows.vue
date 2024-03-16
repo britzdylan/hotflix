37d62e5b11f337c62018a8c2921bb1e81cac5bec
Dylan <britzdylan@gmail.com>
2024-03-16 22:46:07 +0100

    Update New & Hot tv shows header

--- src/views/NewHotShows.vue
+++ src/views/NewHotShows.vue
@@ -4,10 +4,10 @@
       <BaseHeader v-if="windowWidth > 768" />
 
       <HeaderMobileSubPage v-else class="!justify-start">
-        <h2 class="text-xl font-semibold flex-grow">New & Hot tv shows</h2>
+        <h2 class="text-xl font-semibold flex-grow">New & Hot</h2>
       </HeaderMobileSubPage>
     </template>
-    <h2 v-if="windowWidth > 768" class="text-xl font-semibold">New & Hot tv shows</h2>
+    <h2 v-if="windowWidth > 768" class="text-xl font-semibold">New & Hot</h2>
     <ShowGridList :shows="getHotNewShows" />
   </BasePageView>
 </template>
@@ -16,7 +16,6 @@
 import { ref } from 'vue'
 import { useShowsStore } from '@/stores/shows'
 import ShowGridList from '@/components/Show/GridList.vue'
-
 
 const { getHotNewShows } = useShowsStore()
 
