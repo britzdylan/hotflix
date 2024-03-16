0ddf798bf4fcadd1532cd81893abd9d6456a73ee
Dylan <britzdylan@gmail.com>
2024-03-16 01:27:52 +0100

    Update grid layout and add click event to ShowCard

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -4,8 +4,14 @@
       <HeaderSearch />
     </template>
     <h2 class="text-xl font-semibold">Recommended tv shows</h2>
-    <div v-if="!loading" class="grid grid-cols-3 gap-2">
-      <ShowCard class="w-full" v-for="i in 12" :key="i + '_show'"></ShowCard>
+    <div v-if="!loading" class="grid grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4">
+      <ShowCard
+        class="cursor-pointer hover:scale-95 hover:outline outline-2 outline-offset-1 outline-neutral-50 transition-transform duration-300 ease-out"
+        @click="showPopup(`show_${i.id}`)"
+        v-for="i in getHotNewShows"
+        :key="i + '_show'"
+        v-bind="i"
+      ></ShowCard>
     </div>
     <div v-else class="flex justify-center items-center flex-grow h-full">
       <Loader />
@@ -14,11 +20,17 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import BasePageView from '@/components/Base/PageView/index.vue'
 import ShowCard from '@/components/Show/Card.vue'
 import HeaderSearch from '@/components/Header/Search.vue'
 import Loader from '@/components/Icons/Loading.vue'
-import { ref } from 'vue'
+import { useShowsStore } from '@/stores/shows'
+import { usePopup } from '@/composables/popup'
+
+const { showPopup } = usePopup()
+
+const { getHotNewShows } = useShowsStore()
 
 const loading = ref(false)
 </script>
