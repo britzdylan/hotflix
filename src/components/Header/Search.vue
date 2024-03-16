1be47deec0cb438a5e35046ae355abebf9fb1a13
Dylan <britzdylan@gmail.com>
2024-03-16 02:28:14 +0100

    Update search input placeholder

--- src/components/Header/Search.vue
+++ src/components/Header/Search.vue
@@ -2,14 +2,18 @@
   <BaseHeader class="gap-4">
     <IconsChevron @click="$router.back" class="rotate-90" />
     <input
-      type="text"
+      @input="$emit('search', ($event.target as HTMLInputElement)?.value)"
+      type="search"
       class="bg-neutral-700 placeholder:text-neutral-400 border-none text-white w-full px-4 py-3 rounded-md outline-none"
-      placeholder="Search for Names, Genres. & People"
+      placeholder="Search for show names"
     />
   </BaseHeader>
 </template>
 
 <script setup lang="ts">
+import { defineEmits } from 'vue'
 import IconsChevron from '@/components/Icons/Chevron.vue'
 import BaseHeader from './Base.vue'
+
+const emits = defineEmits(['search'])
 </script>
