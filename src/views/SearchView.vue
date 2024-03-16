d22a7f595d4e6621001e1807bc99ae81e9454c4e
Dylan <britzdylan@gmail.com>
2024-03-16 23:27:15 +0100

    Refactor search functionality in SearchView.vue

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -4,7 +4,7 @@
       <HeaderMobileSubPage class="!justify-start">
         <input
           v-model="query"
-          @input="logSearch"
+          @input="startSearch"
           type="search"
           class="bg-neutral-700 placeholder:text-neutral-400 border-none text-white w-full px-4 py-3 rounded-md outline-none"
           placeholder="Search for show names"
@@ -12,61 +12,22 @@
       </HeaderMobileSubPage>
     </template>
     <h2 class="text-xl font-semibold">{{ title }}</h2>
-    <template v-if="showList.length">
-      <ShowGridList v-if="!loading" :shows="showList" />
-      <div v-else class="flex justify-center items-center flex-grow h-full">
-        <Loader />
-      </div>
+    <template v-if="!loading">
+      <ShowGridList v-if="showList.length" :shows="showList" />
+      <div v-else class="flex justify-center items-center flex-grow h-full">No Results</div>
     </template>
     <template v-else>
-      <div class="flex justify-center items-center flex-grow h-full">No Results</div>
+      <div class="flex justify-center items-center flex-grow h-full">
+        <LoaderIcon />
+      </div>
     </template>
   </BasePageView>
 </template>
 
 <script setup lang="ts">
-import type { IShow } from '@/types'
-import { computed, ref } from 'vue'
-import { useShowsStore } from '@/stores/shows'
 import ShowGridList from '@/components/Show/GridList.vue'
 
-const { getRecommendedShows, searchShows } = useShowsStore()
+import { useShowSearch } from '@/composables/showSearch'
 
-const loading = ref(false)
-const isTyping = ref(false)
-const searchResults = ref<IShow[]>([])
-const query = ref('')
-
-let typingTimeout = ref<number | undefined>(undefined)
-
-const logSearch = () => {
-  isTyping.value = true
-  loading.value = true
-
-  if (typingTimeout.value) {
-    clearTimeout(typingTimeout.value)
-  }
-
-  typingTimeout.value = window.setTimeout(async () => {
-    isTyping.value = false
-    searchResults.value = await searchShows(query.value)
-    loading.value = false
-  }, 600)
-}
-
-const showList = computed(() => {
-  if (searchResults.value.length > 0 || query.value.length > 0) {
-    return searchResults.value
-  } else {
-    return getRecommendedShows
-  }
-})
-
-const title = computed(() => {
-  if (searchResults.value.length > 0) {
-    return 'Search results for: ' + query.value
-  } else {
-    return 'Recommended shows'
-  }
-})
+const { query, showList, loading, startSearch, title } = useShowSearch()
 </script>
