16d198c6e0b4e232955c6b6afc5c20f86680b40a
Dylan <britzdylan@gmail.com>
2024-03-16 02:28:29 +0100

    Refactor SearchView.vue to include search functionality

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,10 +1,10 @@
 <template>
   <BasePageView>
     <template #header>
-      <HeaderSearch />
+      <HeaderSearch @search="logSearch" />
     </template>
-    <h2 class="text-xl font-semibold">Recommended tv shows</h2>
-    <ShowGridList v-if="!loading" :shows="getRecommendedShows" />
+    <h2 class="text-xl font-semibold">{{ title }}</h2>
+    <ShowGridList v-if="!loading" :shows="showList" />
     <div v-else class="flex justify-center items-center flex-grow h-full">
       <Loader />
     </div>
@@ -12,14 +12,52 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import type { IShow } from '@/types'
+import { computed, ref } from 'vue'
 import { useShowsStore } from '@/stores/shows'
 import BasePageView from '@/components/Base/PageView/index.vue'
 import HeaderSearch from '@/components/Header/Search.vue'
 import Loader from '@/components/Icons/Loading.vue'
 import ShowGridList from '@/components/Show/GridList.vue'
 
-const { getRecommendedShows } = useShowsStore()
+const { getRecommendedShows, searchShows } = useShowsStore()
 
 const loading = ref(false)
+const isTyping = ref(false)
+const searchResults = ref<IShow[]>([])
+const query = ref('')
+
+let typingTimeout = ref<number | undefined>(undefined)
+
+const logSearch = (search: string) => {
+  isTyping.value = true
+  loading.value = true
+  query.value = search
+
+  if (typingTimeout) {
+    clearTimeout(typingTimeout.value)
+  }
+
+  typingTimeout.value = setTimeout(async () => {
+    isTyping.value = false
+    searchResults.value = await searchShows(search)
+    loading.value = false
+  }, 600)
+}
+
+const showList = computed(() => {
+  if (searchResults.value.length > 0) {
+    return searchResults.value
+  } else {
+    return getRecommendedShows
+  }
+})
+
+const title = computed(() => {
+  if (searchResults.value.length > 0) {
+    return 'Search results for: ' + query.value
+  } else {
+    return 'Recommended shows'
+  }
+})
 </script>
