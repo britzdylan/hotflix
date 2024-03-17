820bd8ae134752980944aafc7139216c2b807004
Dylan <britzdylan@gmail.com>
2024-03-17 16:12:32 +0100

    Add NotFoundView component

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <BasePageView>
+    <template #header>
+      <BaseHeader />
+    </template>
+
+    <h2 class="text-xl font-semibold m-auto">404 - It's a bit lonely here</h2>
+
+    <template #footer>
+      <NavbarMobile />
+    </template>
+  </BasePageView>
+</template>
+
+<script setup lang="ts"></script>
