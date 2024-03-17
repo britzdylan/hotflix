f48b24a2d48a67246d04d7c26f55e04e8912b196
Dylan <britzdylan@gmail.com>
2024-03-17 16:39:01 +0100

    Add popup.spec.ts unit test for popup functionality

---
+++ src/components/__tests__/popup.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect, vi } from 'vitest'
+import { useRoute } from 'vue-router'
+const { usePopup } = await import('@/composables/popup')
+
+let route = {
+  path: '/home',
+  query: {}
+}
+
+const updateRoute = (args: any) => {
+  route = {
+    ...args
+  }
+}
+
+vi.mock('vue-router', () => {
+  return {
+    useRouter: () => {
+      return {
+        replace: (args: any) => updateRoute(args)
+      }
+    },
+    useRoute: () => {
+      return route
+    }
+  }
+})
+
+test('popup', async () => {
+  const { showPopup } = usePopup()
+
+  showPopup('show_1')
+
+  const { popup: newVal, closePopup } = usePopup()
+  expect(newVal.value).toMatchInlineSnapshot(`"show_1"`)
+  expect(route).toMatchInlineSnapshot(`
+    {
+      "path": "/home",
+      "query": {
+        "popup": "show_1",
+      },
+    }
+  `)
+  closePopup()
+
+  const { popup: oldVal } = usePopup()
+  expect(oldVal.value).toMatchInlineSnapshot(`undefined`)
+  expect(route).toMatchInlineSnapshot(`
+    {
+      "path": "/home",
+      "query": {
+        "popup": undefined,
+      },
+    }
+  `)
+})
