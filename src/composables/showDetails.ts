8442f18adccde54be83ab80823603e313a126ada
Dylan <britzdylan@gmail.com>
2024-03-17 16:13:10 +0100

    Refactor showDetails.ts to use useRouter and helper functions

--- src/composables/showDetails.ts
+++ src/composables/showDetails.ts
@@ -1,33 +1,41 @@
 import type { IShowDetailed } from '@/types'
 import { onBeforeMount, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useShowsStore } from '../stores/shows'
 import { usePopup } from '@/composables/popup'
+import { parseShowQueryStringToNumber, parseRouteParamsToNumber } from '@/utils/helpers'
 
 export function useShowDetails() {
-  const { popup, closePopup } = usePopup()
+  const { popup } = usePopup()
+  const router = useRouter()
   const route = useRoute()
   const { getShowById } = useShowsStore()
 
-  const popupId = popup.value ? parseInt(popup.value?.split('_')[1] || '', 10) : null
-  const paramId = route.params.id
-    ? Array.isArray(route.params.id)
-      ? Number(route.params.id[0])
-      : Number(route.params.id)
-    : null
+  const isLoading = ref(true) // test
+  const showDetails = ref<IShowDetailed | null>(null) // test
 
-  const isLoading = ref(true)
-  const showDetails = ref<IShowDetailed | null>(null)
-  const showId = ref(popupId || paramId)
+  const getId = () => {
+    if (popup.value) {
+      return parseShowQueryStringToNumber(popup.value)
+    }
+    if (route.params.id) {
+      return parseRouteParamsToNumber(route.params.id)
+    }
+    return null
+  }
 
   onBeforeMount(async () => {
-    if (!showId.value) return
-    showDetails.value = await getShowById(showId.value)
-    isLoading.value = false
+    if (getId() === null) {
+      isLoading.value = false
+      router.push({ name: 'NotFoundView' })
+    } else {
+      showDetails.value = await getShowById(getId() as Number)
+      isLoading.value = false
+    }
   })
 
   const getImages = (type: 'poster' | 'background') =>
     showDetails.value?.images.filter((i) => i.type === type).map((i) => i.image) || []
 
-  return { isLoading, showDetails, getImages, closePopup }
+  return { isLoading, showDetails, getImages }
 }
